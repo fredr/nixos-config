3c9ff521e8969b5b7957bb37ae1bebe811f08cc5
Fredrik Enestad <fredrik@enestad.com>
2024-04-17 22:33:38 +0200

    continue sway config

--- home-manager/sway.nix
+++ home-manager/sway.nix
@@ -1,11 +1,28 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-    swayidle
-    swaylock
     wl-clipboard
   ];
 
-  wayland.windowManager.sway = {
+  programs.rofi = {
+    enable = true;
+    package = pkgs.rofi;
+
+    extraConfig = {
+      display-drun = "Application";
+      display-window = "Windows";
+      drun-display-format = "{name}";
+      modi = "window,drun,run";
+      show-icons = true;
+    };
+  };
+
+  programs.swaylock.enable = true;
+
+  services.swayidle.enable = true;
+
+  wayland.windowManager.sway = let
+    mod = "Mod4";
+  in {
     enable = true;
 
     extraSessionCommands = ''
@@ -13,19 +30,37 @@
       export SSH_AUTH_SOCK
     '';
 
+    extraConfig = ''
+      set $windowswitcher 'rofi -show window'
+      bindsym ${mod}+Tab exec $windowswitcher
+    '';
+
     config = rec {
-      modifier = "Mod4";
+      modifier = mod;
       terminal = "alacritty"; 
+
+      left = "h";
+      down = "j";
+      up = "k";
+      right = "l";
+
+      menu = "'${pkgs.rofi}/bin/rofi -modi drun,window,run -show drun'";
 
       gaps = {
         smartGaps = true;
         inner = 2;
+        outer = 4;
       };
 
       input = {
         "type:touchpad" = {
           natural_scroll = "enabled";
 	};
+        "*" = {
+	  xkb_layout = "us,se";
+	  xkb_variant = "intl,";
+	  xkb_options = "grp:alt_space_toggle";
+        };
       };
 
       output = {
