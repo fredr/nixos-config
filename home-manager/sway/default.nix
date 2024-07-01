2a7dac1a8e5ceb4fb736106467ceef3d0c7daecb
Fredrik Enestad <fredrik@enestad.com>
2024-07-01 09:08:50 +0200

    fix resume command

--- home-manager/sway/default.nix
+++ home-manager/sway/default.nix
@@ -30,11 +30,10 @@
 
     timeouts = [
       { timeout = 300; command = "${pkgs.chayang}/bin/chayang && ${pkgs.swaylock}/bin/swaylock -f -c 000000"; }
-      { timeout = 600; command = "${pkgs.sway}/bin/swaymsg \"output * dpms off\""; }
+      { timeout = 600; command = "${pkgs.sway}/bin/swaymsg \"output * dpms off\""; resumeCommand = "${pkgs.sway}/bin/swaymsg \"output * dpms on\""; }
       { timeout = 900; command = "${pkgs.systemd}/bin/systemctl suspend"; }
     ];
     events = [
-      { event = "after-resume"; command = "${pkgs.sway}/bin/swaymsg \"output * dpms on\""; }
       { event = "before-sleep"; command = "${pkgs.swaylock}/bin/swaylock -f -c 000000"; }
     ];
 
