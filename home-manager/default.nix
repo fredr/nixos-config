5b89acf07c8b842ad5b61644cd65efedad0c93b0
Fredrik Enestad <fredrik@enestad.com>
2024-07-19 09:59:00 +0200

    some zed rust config

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -81,9 +81,34 @@ in
       "ui_font_size": 16,
       "buffer_font_size": 16,
       "vim_mode": true,
+      "auto_update": false,
+      "relative_line_numbers": true,
       "lsp": {
         "rust-analyzer": {
           "initialization_options": {
+            "completion": {
+              "snippets": {
+                "Arc::new": {
+                    "postfix": "arc",
+                    "body": ["Arc::new(''${receiver})"],
+                    "requires": "std::sync::Arc",
+                    "scope": "expr"
+                },
+                "Some": {
+                    "postfix": "some",
+                    "body": ["Some(''${receiver})"],
+                    "scope": "expr"
+                },
+                "Ok": {
+                    "postfix": "ok",
+                    "body": ["Ok(''${receiver})"],
+                    "scope": "expr"
+                },
+              },
+            },
+            "rust": {
+              "analyzerTargetDir": true,
+            },
             "check": {
               "command": "clippy"
             }
