61d8e55b630c9a94b7fa57833b3882cd016fbc19
Fredrik Enestad <fredrik@enestad.com>
2024-07-26 14:14:38 +0200

    set tabsize

--- home-manager/neovim/init.lua
+++ home-manager/neovim/init.lua
@@ -8,6 +8,9 @@ vim.cmd.colorscheme 'nordic'
 vim.opt.list = false
 vim.opt.listchars = { tab = "→ ", space = "·", trail = "·", nbsp = "␣", eol = "↴" }
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
