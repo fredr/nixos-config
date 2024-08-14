36bc4f398961d075ee522ee48b346ea5956d0eb6
Fredrik Enestad <fredrik@enestad.com>
2024-08-14 14:55:35 +0200

    enable docker unix socket

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -115,7 +115,7 @@
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
-      #dockerSocket.enable = true;
+      dockerSocket.enable = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
